74931d8b1c5aeafe02e3e753938924abb8778170
sean-mahan <seanmahansean@gmail.com>
2021-09-28 19:44:06 -0500

    3 down 2 to go

--- api/server.js
+++ api/server.js
@@ -1,3 +1,73 @@
-// BUILD YOUR SERVER HERE
+const express = require("express")
+const User = require("./users/model")
+const server = express()
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+server.post("/api/users", (req, res) => {
+  const user = req.body
+  if(!user.name || !user.bio){
+    res.status(400).json({
+      message: "Please provide name and bio for the user"
+    })
+  }else{
+    User.insert(user)
+      .then(nUser => {
+        res.status(201).json(nUser)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+          err: err.message
+        })
+      })
+  }
+})
+
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+        err: err.message
+      })
+    })
+})
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if(user){
+        res.json(user)
+      }else{
+        res.status(404).json({
+          message: "The user with the specified ID does not exist"
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The user information could not be retrieved",
+        err: err.message
+      })
+    })
+})
+/*
+server.delete("/api/users/:id", (req, res) => {
+  User.delete
+})
+
+server.put("/api/users/:id", (req, res) => {
+  User.update
+})
+*/
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "Not Found"
+  })
+})
+
+module.exports = server
